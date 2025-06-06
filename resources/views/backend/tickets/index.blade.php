dcb067c3940cb719c40bbbc7cd8ad719af79af83
Douglas Quele <douglas.quele21@gmail.com>
2025-06-05 21:51:15 -0600

    prueba

--- resources/views/backend/tickets/index.blade.php
+++ resources/views/backend/tickets/index.blade.php
@@ -34,7 +34,7 @@
                 <thead class="table-dark">
                     <!--Columnas-->
                     <tr>
-                        <th>ID</th>
+                        <th>ID TICKET</th>
                         <th>Título</th>
                         <th>Usuario</th>
                         <th>Estado</th>
